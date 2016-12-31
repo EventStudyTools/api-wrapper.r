ad3e00daf5f8e7c37fa9a8bce0837ee107851829
Simon Müller <info@muon-stat.com>
2016-12-31 23:30:28 +0100

    update description

--- tests/testthat/test-ApplicationInputInterface.R
+++ tests/testthat/test-ApplicationInputInterface.R
@@ -2,9 +2,4 @@ context("Application Input Interface")
 
 test_that("Application Input Interface", {
 
-  AII <- ApplicationInputInterface$new()
-  parameterList <- list("arc" = 1,
-                        "test2" = 1)
-  AII$setNamedList(parentLevel = "tasks", parameterList = parameterList)
-  AII$tasks
 })
