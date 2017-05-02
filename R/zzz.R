9f92a5ed63cae2f7bb6db9da8b0f120a5f27b73b
Simon Müller <info@muon-stat.com>
2017-05-02 23:22:27 +0200

    update onload options

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,24 @@
+.onLoad <- function(libname, pkgname) {
+  
+  op <- options()
+  op.EventStudy <- list(
+    EventStudy.URL = "http://api.eventstudytools.com",
+    EventStudy.verbose = 3,
+    EventStudy.tryAttempts = 5
+  )
+  toset <- !(names(op.EventStudy) %in% names(op))
+  if(any(toset)) options(op.EventStudy[toset])
+  invisible()
+}
+
+.onAttach <- function(libname, pkgname) {
+  
+  default_scopes <- getOption("googleAuthR.scopes.selected")
+  
+  googleAuthR::gar_attach_auto_auth(default_scopes)
+  
+}
+
 .onAttach = function(libname, pkgname) {
   if (interactive()) {
     packageStartupMessage("EventStudy is still in beta version - APIs can be changed.")
